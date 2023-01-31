81f32aa9171bd86b18acad837c0addefc97c0c9c
Pavel Demaev <ctpater@gmail.com>
2023-01-31 20:04:01 +0300

    feat: add side menu

--- client/src/application/core/components/main-container/main-container.ts
+++ client/src/application/core/components/main-container/main-container.ts
@@ -1,10 +1,15 @@
 import './main-container.scss';
 import { DOMElement } from '../../../shared/components/base-elements/dom-element';
+import { SideMenu } from './side-menu/side-menu';
 
 export class Main extends DOMElement {
   private mainContainer: DOMElement;
 
+  private sideMenu: DOMElement;
+
   public container: HTMLElement;
+
+  private sidemenu: SideMenu;
 
   constructor(parentNode: HTMLElement) {
     super(parentNode, {
@@ -17,5 +22,12 @@ export class Main extends DOMElement {
       classList: ['container', 'main__container'],
     });
     this.container = this.mainContainer.node;
+
+    this.sideMenu = new DOMElement(this.mainContainer.node, {
+      tagName: 'nav',
+      classList: ['nav__side-menu'],
+    });
+
+    this.sidemenu = new SideMenu(this.sideMenu.node);
   }
 }
