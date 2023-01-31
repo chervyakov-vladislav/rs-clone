ae1d867f2c30c0dcbb151cbe6f117b28ef40652f
Vladislav Chervyakov <chervyakovve@mail.ru>
2023-01-31 22:38:59 +0700

    fix: login style fix

--- client/src/application/core/components/header/login/login.ts
+++ client/src/application/core/components/header/login/login.ts
@@ -1,14 +1,22 @@
 import './login.scss';
 import { ButtonElement } from '../../../../shared/components/base-elements/button-element';
 import { LoginMenu } from './login-menu/login-menu';
+import { DOMElement } from '../../../../shared/components/base-elements/dom-element';
 
-export class Login extends ButtonElement {
+export class Login extends DOMElement {
   private menu: LoginMenu;
+
+  private loginBtn: ButtonElement;
 
   constructor(parentNode: HTMLElement) {
     super(parentNode, {
-      tagName: 'button',
+      tagName: 'div',
       classList: ['login'],
+    });
+
+    this.loginBtn = new ButtonElement(this.node, {
+      tagName: 'button',
+      classList: ['login__button'],
       content: 'Вход',
     });
 
