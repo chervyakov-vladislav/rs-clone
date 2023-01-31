431e7aadcbea0ef1dee1352533d60037bca12fb9
Vladislav Chervyakov <chervyakovve@mail.ru>
2023-01-31 22:49:36 +0700

    refactor: add button&link tag

--- client/src/application/core/components/header/login/login-menu/login-menu.ts
+++ client/src/application/core/components/header/login/login-menu/login-menu.ts
@@ -1,10 +1,16 @@
 import './login-menu.scss';
 import { DOMElement } from '../../../../../shared/components/base-elements/dom-element';
+import { LinkElement } from '../../../../../shared/components/base-elements/link-element';
+import { ButtonElement } from '../../../../../shared/components/base-elements/button-element';
 
 export class LoginMenu extends DOMElement {
-  private accauntLink: DOMElement;
+  private accountItem: DOMElement;
 
-  private exitButton: DOMElement;
+  private accountLink: LinkElement;
+
+  private exitItem: DOMElement;
+
+  private exitButton: ButtonElement;
 
   constructor(parentNode: HTMLElement) {
     super(parentNode, {
@@ -12,15 +18,26 @@ export class LoginMenu extends DOMElement {
       classList: ['login-menu'],
     });
 
-    this.accauntLink = new DOMElement(this.node, {
+    this.accountItem = new DOMElement(this.node, {
       tagName: 'li',
       classList: ['login-menu__item'],
-      content: 'Личный кабинет',
     });
 
-    this.exitButton = new DOMElement(this.node, {
+    this.accountLink = new LinkElement(this.accountItem.node, {
+      tagName: 'a',
+      classList: ['login-menu__link'],
+      content: 'Личный кабинет',
+      href: '#',
+    });
+
+    this.exitItem = new DOMElement(this.node, {
       tagName: 'li',
       classList: ['login-menu__item'],
+    });
+
+    this.exitButton = new ButtonElement(this.exitItem.node, {
+      tagName: 'button',
+      classList: ['login-menu__link'],
       content: 'Выйти',
     });
   }
