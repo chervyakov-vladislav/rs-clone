88fe8eb08675b14178528ed93d9bc2178c6e24d8
Pavel Demaev <ctpater@gmail.com>
2023-02-01 19:14:12 +0300

    fix: change console.log to return, rename variable

--- client/src/application/shared/services/api-kinopoisk/api-kinopoisk.ts
+++ client/src/application/shared/services/api-kinopoisk/api-kinopoisk.ts
@@ -1,5 +1,5 @@
 class ApiServiceKinopoisk {
-  apiKey = '1eed0f50-3f3b-49f1-b171-1547c878ae42';
+  private apiKey = '1eed0f50-3f3b-49f1-b171-1547c878ae42';
 
   private baseUrl: string = 'https://kinopoiskapiunofficial.tech/api';
 
@@ -9,7 +9,7 @@ class ApiServiceKinopoisk {
 
   // Выбор топ фильмов TOP_100_POPULAR_FILMS, TOP_250_BEST_FILMS, TOP_AWAIT_FILMS
   // Возвращает список фильмов с пагинацией. Каждая страница содержит не более чем 20 фильмов.
-  private getTop: string = `${this.filmData}/top?type=TOP_100_POPULAR_FILMS&page=`;
+  private topFilms: string = `${this.filmData}/top?type=TOP_100_POPULAR_FILMS&page=`;
 
   public async getFilmData(id: number): Promise<void> {
     const response = await fetch(`${this.filmData}/${id}`, {
@@ -19,7 +19,7 @@ class ApiServiceKinopoisk {
       },
     });
     const data = await response.json();
-    console.log(data);
+    return data;
   }
 
   // Данный эндпоинт возвращает изображения связанные с фильмом с пагинацией.
@@ -42,7 +42,7 @@ class ApiServiceKinopoisk {
       },
     });
     const data = await response.json();
-    console.log(data);
+    return data;
   }
 
   public async getFilmReviews(id: number): Promise<void> {
@@ -53,7 +53,7 @@ class ApiServiceKinopoisk {
       },
     });
     const data = await response.json();
-    console.log(data);
+    return data;
   }
 
   public async getFilmVideos(id: number): Promise<void> {
@@ -64,7 +64,7 @@ class ApiServiceKinopoisk {
       },
     });
     const data = await response.json();
-    console.log(data);
+    return data;
   }
 
   public async getFilmBoxOffice(id: number): Promise<void> {
@@ -75,7 +75,7 @@ class ApiServiceKinopoisk {
       },
     });
     const data = await response.json();
-    console.log(data);
+    return data;
   }
 
   public async getFilmStaff(id: number): Promise<void> {
@@ -86,18 +86,18 @@ class ApiServiceKinopoisk {
       },
     });
     const data = await response.json();
-    console.log(data);
+    return data;
   }
 
   public async getTopData(page = 1): Promise<void> {
-    const response = await fetch(`${this.getTop}${page}`, {
+    const response = await fetch(`${this.topFilms}${page}`, {
       headers: {
         Accept: 'application/json',
         'X-Api-Key': this.apiKey,
       },
     });
     const data = await response.json();
-    console.log(data);
+    return data;
   }
 }
 
