510b2a923bb8ecabc8687c2fbd51f3250253e747
github-usernameEXisTAnZ <ozim_ssspam@mail.ru>
2023-02-02 15:33:20 +0300

    feat: add express server with usersRouter

---
+++ server/src/app/server.ts
@@ -0,0 +1,21 @@
+import e from "express";
+import { Express } from "express";
+import UsersRouter from "./main/components/users/users";
+
+export default class Server {
+  private PORT: string;
+  private app: Express;
+  private usersRouter: UsersRouter;
+
+  constructor() {
+    this.PORT = process.env.PORT || '3000';
+    this.app = e();
+    this.usersRouter = new UsersRouter();
+  }
+
+  public start() {
+    this.app.use('/api/user', this.usersRouter.router);
+    this.app.listen(this.PORT)
+    console.info(`Server is started on port ${this.PORT}`);
+  }
+}
