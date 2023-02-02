925b1e37036c4a657d0d05cdd9fab97330cc45c1
github-usernameEXisTAnZ <ozim_ssspam@mail.ru>
2023-02-02 15:08:58 +0300

    fix: change types of requset response express

--- server/src/app/main/components/users/users.ts
+++ server/src/app/main/components/users/users.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import { Request, Response } from 'express-serve-static-core';
-import { ParsedQs } from 'qs';
 import { User } from '../../../shared/types';
 
 export default class UsersRouter {
@@ -11,12 +10,12 @@ export default class UsersRouter {
     this.router.post('/register', (req, res) => this.register(req ,res));
   }
 
-  private login(req: Request<{}, any, any, ParsedQs, Record<string, any>> , res: Response<any, Record<string, any>, number>) {
+  private async login(req: Request , res: Response) {
     console.log(req.body);
     res.send('Login succeed!')
   }
 
-  private register(req: Request<{}, any, any, ParsedQs, Record<string, any>> , res: Response<any, Record<string, any>, number>) {
+  private async register(req: Request , res: Response) {
     console.log(req.body);
     res.send('Rigistered!')
   }
