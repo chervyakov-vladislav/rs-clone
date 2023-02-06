aa1241e137e610e109c060f5de18afb14f5964b5
github-usernameEXisTAnZ <ozim_ssspam@mail.ru>
2023-02-06 16:14:22 +0300

    refactor: add inpuNode to InputElement

--- client/src/application/shared/components/base-elements/input-element.ts
+++ client/src/application/shared/components/base-elements/input-element.ts
@@ -4,66 +4,69 @@ import { InputOptions } from '../../models/base-elements';
 export default class InputElement extends DOMElement {
   private _value = '';
 
+  public inputNode: HTMLInputElement;
+
   constructor(parentNode: HTMLElement | null, options: InputOptions) {
     super(parentNode, {
       tagName: options.tagName,
       classList: options.classList,
       content: options.content,
     });
+    this.inputNode = this.node as HTMLInputElement;
 
     if (options.type) {
-      this.node.setAttribute('type', options.type);
+      this.inputNode.type = options.type;
     }
 
     if (options.name) {
-      this.node.setAttribute('name', options.name);
+      this.inputNode.name = options.name;
     }
 
     if (options.placeholder) {
-      this.node.setAttribute('placeholder', options.placeholder);
+      this.inputNode.placeholder = options.placeholder;
     }
 
     if (options.required) {
-      (this.node as HTMLInputElement).required = true;
+      this.inputNode.required = options.required;
     }
 
     if (options.step) {
-      (this.node as HTMLInputElement).step = options.step;
+      this.inputNode.step = options.step;
     }
 
     if (options.selected) {
-      (this.node as HTMLOptionElement).selected = true;
+      (this.inputNode as HTMLElement as HTMLOptionElement).selected = true;
     }
 
     if (options.min) {
-      this.node.setAttribute('min', options.min);
+      this.inputNode.min = options.min;
     }
 
     if (options.max) {
-      this.node.setAttribute('max', options.max);
+      this.inputNode.max = options.max;
     }
 
     if (options.value) {
-      this.node.setAttribute('value', options.value);
+      this.inputNode.value = options.value;
       this._value = options.value;
     }
 
     if (options.checked) {
-      (this.node as HTMLInputElement).checked = options.checked;
+      this.inputNode.checked = options.checked;
     }
 
     if (options.readonly) {
-      (this.node as HTMLInputElement).readOnly = options.readonly;
+      this.inputNode.readOnly = options.readonly;
     }
   }
 
   public set value(val: unknown) {
     if (typeof val === 'string') this._value = val;
     if (typeof val === 'number') this._value = val.toString();
-    (this.node as HTMLInputElement).value = this._value;
+    this.inputNode.value = this._value;
   }
 
-  public get value() {
+  public get value(): string {
     return this._value;
   }
 }
