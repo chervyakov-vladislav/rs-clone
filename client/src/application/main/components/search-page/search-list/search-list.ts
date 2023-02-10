db849406d67b2cdb403db6bdf555f6dd25702ccf
Vladislav Chervyakov <chervyakovve@mail.ru>
2023-02-10 21:08:21 +0700

    feat: add throttle loading

--- client/src/application/main/components/search-page/search-list/search-list.ts
+++ client/src/application/main/components/search-page/search-list/search-list.ts
@@ -1,6 +1,8 @@
 import './search-list.scss';
 import DOMElement from '../../../../shared/components/base-elements/dom-element';
 import renderCards from '../../../services/search-page/render-cards/render-card.service';
+import infiniteScroll from '../../../services/search-page/inifinite-scroll/infinite-scroll.service';
+import state from '../../../../shared/services/state';
 
 export default class SearchList extends DOMElement {
   constructor(parentNode: HTMLElement) {
@@ -10,5 +12,14 @@ export default class SearchList extends DOMElement {
     });
     renderCards.setContainer(this.node);
     renderCards.render();
+    const func = infiniteScroll.scrollListener.bind(infiniteScroll);
+    window.addEventListener('scroll', () => {
+      const { currentPageID } = state.getPreviousPageInfo();
+      if (currentPageID === 'searchPage') func();
+    });
+    window.addEventListener('resize', () => {
+      const { currentPageID } = state.getPreviousPageInfo();
+      if (currentPageID === 'searchPage') func();
+    });
   }
 }
--- client/src/application/shared/services/api/api-helpers.service.ts
+++ client/src/application/shared/services/api/api-helpers.service.ts
@@ -1,14 +1,31 @@
 class ApiHelpers {
-  private timeout: NodeJS.Timeout | number = 0;
+  private debounceTimeout: NodeJS.Timeout | number = 0;
+
+  private throttleTimeout: NodeJS.Timeout | number = 0;
 
   public debounce<F extends (...args: Parameters<F>) => ReturnType<F>>(
     func: F,
     ms = 1_000
   ): (...args: Parameters<F>) => void {
     return (...args: Parameters<F>): void => {
-      clearTimeout(this.timeout);
-      this.timeout = setTimeout(() => {
+      clearTimeout(this.debounceTimeout);
+      this.debounceTimeout = setTimeout(() => {
         func.apply(this, args);
+      }, ms);
+    };
+  }
+
+  public throttle<F extends (...args: Parameters<F>) => ReturnType<F>>(
+    func: F,
+    ms = 2_000
+  ): (...args: Parameters<F>) => void {
+    return (...args: Parameters<F>): void => {
+      if (this.throttleTimeout) return;
+      func.apply(this, args);
+
+      this.throttleTimeout = setTimeout(() => {
+        clearTimeout(this.throttleTimeout);
+        this.throttleTimeout = 0;
       }, ms);
     };
   }
