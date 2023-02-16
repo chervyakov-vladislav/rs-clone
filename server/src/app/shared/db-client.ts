53a5cd3ca5d7ef20d3aacb723ce5b32ee20fba81
EXisTAnZ <ozim_ssspam@mail.ru>
2023-02-16 16:59:49 +0300

    feat: add mongodb client

---
+++ server/src/app/shared/db-client.ts
@@ -0,0 +1,39 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  private DBName: string;
+
+  private DBUser: string;
+
+  private DBPassword: string;
+
+  private client: MongoClient;
+
+  constructor() {
+    this.DBName = 'rsclone';
+    this.DBUser = process.env.DB_USER||'rs-clone';
+    this.DBPassword = process.env.DB_PASSWORD||'Z94ai17';
+    const uri = `mongodb+srv://${this.DBUser}:${this.DBPassword}@cluster0.2uewvbb.mongodb.net/?retryWrites=true&w=majority`;
+    this.client = new MongoClient(uri);
+  }
+
+  public async connect() {
+    try {
+      this.client.connect();
+    } catch (error) {
+      console.log(error);
+    }
+    console.info(`mongo db client is connected to ${this.DBName}`);
+        this.client.on(`close`, () => {
+          console.info(`mongo db client is disconnected`);
+        });
+    return this.client.db(this.DBName);
+  }
+
+  public async getUsersCollection() {
+      return (await this.connect()).collection('users')
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
