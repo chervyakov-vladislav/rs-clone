9d0def41693f344f53d0f6f1578faa0109eccab7
EXisTAnZ <ozim_ssspam@mail.ru>
2023-02-21 22:32:06 +0300

    feat: update user on submit form

--- client/src/application/main/components/account-page/user-data/user-data.ts
+++ client/src/application/main/components/account-page/user-data/user-data.ts
@@ -10,6 +10,8 @@ import LikedFilms from './liked-films/liked-films';
 import ReviewsFilms from './reviews/reviews';
 import loginObserver from '../../../../core/services/menu/login-observer.service';
 import WatchLater from './watch-later/watch-later';
+import apiService from '../../../../shared/services/api/server-api.service';
+import { User } from '../../../../shared/models/state';
 
 export default class UserData extends DOMElement {
   private title: DOMElement;
@@ -48,13 +50,17 @@ export default class UserData extends DOMElement {
 
   private clearLocalStorage: ButtonElement;
 
+  private avatar: File | string;
+
+  private data: User;
+
   constructor(parentNode: HTMLElement) {
     super(parentNode, {
       tagName: 'div',
       classList: ['user-data'],
     });
-    const data = state.getUserData();
-
+    this.data = state.getUserData();
+    this.avatar = this.data.userPhoto;
     this.title = new DOMElement(this.node, {
       tagName: 'h1',
       classList: ['user-data__title'],
@@ -79,7 +85,7 @@ export default class UserData extends DOMElement {
     this.userPhoto = new ImageElement(this.userPicWrapper.node, {
       tagName: 'img',
       classList: ['user-data__image'],
-      src: `${data.userPhoto}`,
+      src: `${this.data.userPhoto}`,
     });
 
     this.uploadButton = new DOMElement(this.imageContainer.node, {
@@ -96,7 +102,7 @@ export default class UserData extends DOMElement {
     });
 
     this.loadImage.node.addEventListener('change', (e: Event) => {
-      const file = Array.from((e.target as HTMLInputElement).files as FileList)[0];
+      [this.avatar] = Array.from((e.target as HTMLInputElement).files as FileList);
       const reader = new FileReader();
       reader.addEventListener('load', async (event: Event) => {
         const { result } = event.target as FileReader;
@@ -104,8 +110,8 @@ export default class UserData extends DOMElement {
         loginObserver.setButtonText();
         (this.userPhoto.node as HTMLImageElement).src = `${result}`;
       });
-      if (file.size < 5242880) {
-        reader.readAsDataURL(file);
+      if (this.avatar.size < 5242880) {
+        reader.readAsDataURL(this.avatar);
         this.userValidationMassage.node.innerHTML = '';
       } else {
         this.userValidationMassage.node.innerHTML = 'Файл должен быть меньше 5мб';
@@ -117,19 +123,22 @@ export default class UserData extends DOMElement {
       classList: ['user-data__name-password'],
       action: '#account',
     });
-    this.userForm.node.addEventListener('submit', userValidation.submit.bind(userValidation));
+    this.userForm.node.addEventListener('submit', (e) => {
+      userValidation.submit(e);
+      this.updateUser();
+    });
 
     this.loginInfo = new DOMElement(this.userForm.node, {
       tagName: 'div',
       classList: ['user-data__ligin-info'],
-      content: `Ваш логин для входа в аккаунт: ${data.userName}`,
+      content: `Ваш логин для входа в аккаунт: ${this.data.userLogin}`,
     });
 
     this.userNameInput = new InputElement(this.userForm.node, {
       tagName: 'input',
       classList: ['user-data__text-input'],
       placeholder: 'Имя пользователя',
-      value: data.userName,
+      value: this.data.userName,
     });
 
     this.userOldPassInput = new InputElement(this.userForm.node, {
@@ -190,5 +199,17 @@ export default class UserData extends DOMElement {
     setTimeout(() => {
       this.reviews = new ReviewsFilms(this.node);
     }, 2_000);
+  }
+
+  private updateUser() {
+    apiService.updateUser(
+      {
+        login: this.data.userLogin,
+        name: this.userNameInput.inputNode.value,
+        password: this.userOldPassInput.inputNode.value,
+        role: '',
+      },
+      this.avatar instanceof File ? this.avatar : undefined
+    );
   }
 }
