4eee93643474c33bc5fed20c38d641071eca9909
EXisTAnZ <ozim_ssspam@mail.ru>
2023-02-24 16:17:16 +0300

    feat: add Post type

--- server/src/app/shared/model/types.ts
+++ server/src/app/shared/model/types.ts
@@ -6,6 +6,17 @@ export type User = {
   avatar?: string;
 };
 
+export type Post = {
+  author: string;
+  date: string;
+  description: string;
+  kinopoiskId: number;
+  negativeRating: number;
+  positiveRating: number;
+  title: string;
+  type: string;
+};
+
 export type Premiere = {
   ID: string;
   link: string;
