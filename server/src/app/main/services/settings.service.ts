7d6e51a8a74d6219024fa4a7719c5ee90f0625bc
EXisTAnZ <ozim_ssspam@mail.ru>
2023-02-25 19:07:14 +0300

    refactor: removed unused properties

--- server/src/app/main/services/settings.service.ts
+++ server/src/app/main/services/settings.service.ts
@@ -1,14 +1,10 @@
-import { MongoClient } from 'mongodb';
 import dbClient from '../../shared/db-client';
 import { Premiere } from '../../shared/model/types';
 
 export default class SettingService {
-  private mongoClient: MongoClient;
-
   private premiere: Premiere;
 
   constructor() {
-    this.mongoClient = new MongoClient('mongodb+srv://rs-clone');
     this.premiere = {
       ID: '301',
       link: 'https://youtu.be/8qB8EGNOtr8',
