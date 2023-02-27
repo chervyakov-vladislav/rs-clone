7c2fd4cb8dc786f8140a60d8131e13f644ed9003
EXisTAnZ <ozim_ssspam@mail.ru>
2023-02-27 09:43:32 +0300

    fix: bylogin rout now gets only selected users posts

--- server/src/app/main/components/posts/posts.ts
+++ server/src/app/main/components/posts/posts.ts
@@ -3,18 +3,22 @@ import { Request, Response } from 'express-serve-static-core';
 import { validationResult } from 'express-validator';
 import { postValidation } from '../../../shared/model/validations';
 import PostsService from '../../services/post.service';
+import UsersService from '../../services/users.service';
 
 export default class PostsRouter {
   public router = express.Router();
 
   private postsService: PostsService;
 
+  private usersService: UsersService;
+
   constructor() {
     this.postsService = new PostsService();
+    this.usersService = new UsersService();
     this.router.post('/new/:filmID', postValidation, (req: Request, res: Response) => this.newPost(req, res));
     this.router.get('/all', (req: Request, res: Response) => this.getAllPosts(req, res));
     this.router.get('/byfilm/:filmID', (req: Request, res: Response) => this.getPostsbyFilmID(req, res));
-    this.router.get('/bylogin/:login', (req: Request, res: Response) => this.getAllPosts(req, res));
+    this.router.get('/bylogin/:login', (req: Request, res: Response) => this.getPostsbyLogin(req, res));
   }
 
   private async newPost(req: Request, res: Response) {
@@ -24,12 +28,16 @@ export default class PostsRouter {
       return res.status(400).json({ errors: errors.array()[0] });
     }
     try {
-      const posts = await this.postsService.create(req.body, req.params.filmID);
-      console.log(req.params.filmID, posts);
+      const login = this.usersService.verifyToken(req.headers.authorization || '');
+      if (!login) {
+        throw new Error('Пользователь неавторизован');
+      }
+      const post = await this.postsService.create(req.body, req.params.filmID, login);
+      console.log(req.params.filmID, login, post);
       res.send({
         errors: null,
         token: '',
-        data: posts,
+        data: post,
       });
     } catch (err) {
       console.log(err);
